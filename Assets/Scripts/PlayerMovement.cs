ba5676d09792382253b61420bb02cacad2fe0843
Jason Castillon <jcasti42@stedwards.edu>
2025-02-18 14:59:38 -0600

    updated starting point of project
    added some level design for

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,149 +2,241 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    /*This basic movement code comes from "FIRST PERSON MOVEMENT in 10 MINUTES - Unity Tutorial" by "Dave / GameDevelopment" on YouTube.
-    I've totally over-commented it so that y'all can understand all the moving parts here, but as you work with it, if the comments are distracting, just delete them!
-    If you'd like to see him build this code from the ground up in a succinct 10 minute tutorial, please watch the original video.
-    This code is designed to be simple and EXPANDABLE and in further videos he adds aditional movement options that you can add too.
-    He expands on this code to include slope movement, sprinting, crouching, throwing, sliding, climbing, dashing, and grappling hook swinging.
-    So if any of that sounds like something you'd like in your game, go ahead and check out his videos. */
-
 public class PlayerMovement : MonoBehaviour
 {
-    //Headers are a great way to keep your Inspector organized if you're going to have lots of public, exposed variables
     [Header("Movement")]
-    public float moveSpeed; //Public so we'll be able to customize this in the inspector
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
 
-    public float groundDrag; //essentially how much friction we'll have on the floor.
+    public float groundDrag;
 
-    public float jumpForce; //How much force we exert on the rigidbody when we jump. Basically ike jump speed/height 
-    public float jumpCooldown; //How long until we can jump again
-    public float airMultiplier; //kind of like our air resistance
-    bool readyToJump; //True if we can jump, false if not
+    [Header("Jumping")]
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
 
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
-    [Header("Ground Check")] //We're gonna check when we're touching the ground for a couple of reasons like knowing when we can jump and when to apply drag
-    public float playerHeight; //so we can tell where the ground is relative to the player's center
-    public LayerMask whatIsGround; //we'll apply this layer to any "Ground" objects in our scene
-    bool grounded; //This will be true if we're touching the ground, false if we're not
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
 
 
-    public Transform orientation; //public because we'll drag and drop our Orientation object in here to capture its transform data (location, rotation, and scale)
+    public Transform orientation;
 
-    //these are Not public because they'll be accepting our keyboard inputs, not something we want visible or customizable in our Inspector
     float horizontalInput;
     float verticalInput;
 
-    Vector3 moveDirection; //A vector3 holds a 3d direction, and a magnitude 
+    Vector3 moveDirection;
 
-    Rigidbody rb; //This will hold a reference to our player's rigidbody. We'll assign it in the code so we don't need it to be public
+    Rigidbody rb;
 
-
-
-    void Start() // Start is called before the first frame update
+    public MovementState state;
+    public enum MovementState
     {
-        rb = GetComponent<Rigidbody>(); //tada now "rb" is a reference to our RigidBody component
-        rb. freezeRotation = true; //we don't want it rotating on its own.
-
-        readyToJump = true; //makes us able to jump when the game start
+        walking,
+        sprinting,
+        crouching,
+        air
     }
 
-
-
-    private void Update() // Update is called once per frame, making it frame dependent and good for inputs
+    private void Start()
     {
-        //this is where we'll check if we're grounded, before letting us input movement, just in case we try to hit the jump button while we're in the air or something
-        // we are grounded if when we shoot a raycast from our center to just below us (half our height plus a little extra) we hit something with our ground layer
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
 
-        MyInput(); //Every frame we're gonna collect our inputs using a custom method of our own creation
-    
-        SpeedControl(); //call our function to cap our max movement speed
+        readyToJump = true;
 
+        startYScale = transform.localScale.y;
+    }
 
-        //handle drag
-        if(grounded)
-        {
+    private void Update()
+    {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+        StateHandler();
+
+        // handle drag
+        if (grounded)
             rb.linearDamping = groundDrag;
-        }
         else
-        {
             rb.linearDamping = 0;
-        }
     }
 
-
-    private void FixedUpdate() //FixedUpdate is called at a constant REAL WORLD rate, indpendent of rame rate, making it good for physics calculations
+    private void FixedUpdate()
     {
-        MovePlayer(); //We are gonna run our custom physics method continuously and at a constant rate
+        MovePlayer();
     }
 
-
-    private void MyInput() //this is our own custom method. Private because no other scripts need to call it, and void because it won't return a value when run
+    private void MyInput()
     {
-        //Here we're getting our inputs. By default in Unity "Horizontal" is A/D and Left arrow/Right arrow
         horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical"); //Vertical W/S and Up/Down
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        //when to jump
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        // when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
-            
+
             Jump();
 
-            Invoke(nameof(ResetJump), jumpCooldown); //really cool method that lets us call our custom ResetJump method after waiting the length of time of jumpCooldown
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+        // start crouch
+        if (Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        // stop crouch
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
         }
     }
 
+    private void StateHandler()
+    {
+        // Mode - Crouching
+        if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+
+        // Mode - Sprinting
+        else if (grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+
+        // Mode - Walking
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+
+        // Mode - Air
+        else
+        {
+            state = MovementState.air;
+        }
+    }
 
     private void MovePlayer()
     {
-        //calculate movement direction
-        //The direction we move = Y axis direction (forward) * Forward/Backwards input + X axis direction (right) * Left/Right input
+        // calculate movement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        //now that we have our direction, we add it as a force to our rigidbody
-        //normalizing a vector sets its magnitude to 1, so we won't have bigger or smaller vectors, just directions. Force mode force is like continuously pushing on something, rather than one force one time
-        if (grounded) //we have certain movement rules for on the ground, and different ones for in the air
+        // on slope
+        if (OnSlope() && !exitingSlope)
         {
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+
+            if (rb.linearVelocity.y > 0 && state == MovementState.walking)
+            {
+                Debug.Log("adding downwards force while walking up slope");
+                rb.AddForce(Vector3.down * 20f, ForceMode.Force);
+            }
+            else if(rb.linearVelocity.y > 0 && state == MovementState.sprinting)
+            {
+                Debug.Log("adding downwards force while running up slope");
+                rb.AddForce(Vector3.down * 100f, ForceMode.Force);
+            }
+        }
+        // on ground
+        else if (grounded)
+        {
+            Debug.Log("adding force while grounded");
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         }
-        else if (!grounded) //you could probably just say "else" but better safe than sorry
+
+        // in air
+        else if (!grounded)
         {
-            //we just multiply in our air multiplier so we can't move as much in the air as we can on the ground
+            Debug.Log("adding force while in air");
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
         }
+            
+        // turn gravity off while on slope
+        rb.useGravity = !OnSlope();
     }
 
-    //Without this, the player's max speed would be greater than his move speed. We're just gonna put a cap on his max speed
     private void SpeedControl()
     {
-        //We're gonna get the player's "flat velocity", literally just his velocity in the X direction and Z direction without caring how fast he's moving up or down
-        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-
-        //limit velocity if needed
-        if(flatVel.magnitude > moveSpeed)
+        // limiting speed on slope
+        if (OnSlope() && !exitingSlope)
         {
-            //again, normalizing a vector just sets its magnitude to 1 while keeping the direction its facing, then we set its magnitude to our movespeed because that's as fast as we ever want the player going
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z); //you'll notice we're not changing the up down speed. We're only putting a cap on the "flat velocity"
+            if (rb.linearVelocity.magnitude > moveSpeed)
+                rb.linearVelocity = rb.linearVelocity.normalized * moveSpeed;
+        }
+
+        // limiting speed on ground or in air
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            // limit linearVelocity if needed
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
         }
     }
 
     private void Jump()
     {
-        //reset y velocity to 0 just in case
+        exitingSlope = true;
+
+        // reset y linearVelocity
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
 
-        //Add our jump force in the up direction. Force mode impulse is like a single one time force, rather than a continuous pushing force
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
-
     private void ResetJump()
     {
-        readyToJump = true; //let us be able to jump again
+        readyToJump = true;
+
+        exitingSlope = false;
+    }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
     }
 }
