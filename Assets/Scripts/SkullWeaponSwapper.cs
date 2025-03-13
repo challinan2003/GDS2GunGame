250274be30887491c742a6009962fa6f2ef1c0c0
Jason Castillon <jcasti42@stedwards.edu>
2025-03-13 11:46:56 -0500

    fixed weapon swapper
    fixed weapon swapper

--- Assets/Scripts/SkullWeaponSwapper.cs
+++ Assets/Scripts/SkullWeaponSwapper.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponSwap : MonoBehaviour
+public class SkullWeaponSwapper : MonoBehaviour
 {
     public List<GameObject> currentWeapons = new List<GameObject>();
     public int currentWeaponIndex = 0;
@@ -11,23 +11,27 @@ public class WeaponSwap : MonoBehaviour
     {
         for (int i = 0; i < currentWeapons.Count; i++)
         {
+            MeshRenderer meshRenderer = currentWeapons[i].GetComponent<MeshRenderer>();
+            meshRenderer.enabled = false;
             currentWeapons[i].SetActive(false);
         }
+    }
+
+    public void RandomizeWeaponType() {
+        //here is where the collision code will call to switch to a different weapon ability, and possibly call
     }
 
     // Update is called once per frame
     void Update()
     {
         //keep the index between the index range of the weapons list
-        if (currentWeaponIndex < 0) { currentWeaponIndex = currentWeapons.Count - 1; }
+        if(currentWeaponIndex < 0) { currentWeaponIndex = currentWeapons.Count - 1; }
         if (currentWeaponIndex > currentWeapons.Count - 1) { currentWeaponIndex = 0; }
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
             currentWeaponIndex += 1;
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
             currentWeaponIndex -= 1;
         }
 
@@ -37,8 +41,7 @@ public class WeaponSwap : MonoBehaviour
             {
                 currentWeapons[i].SetActive(true);
             }
-            else
-            {
+            else {
                 currentWeapons[i].SetActive(false);
             }
         }
